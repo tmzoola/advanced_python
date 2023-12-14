95d96791e6b0e8ae6e13ffe4198d9573020eadc9
Murodjon Tokhirov <murodtokh1997@gmail.com>
2023-12-14 17:57:46 +0500

    main fileda for loop yozdim

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 text = """Decimal “is based on a floating-point model which was designed with people in mind, and necessarily has a paramount guiding principle – computers must provide an arithmetic that works in the same way as the arithmetic that 
 people learn at school.” – excerpt from the decimal arithmetic specification."""
 
-num = 5
+for i in text:
+    print(i)
 
 
 
