6a9611af71b9bf1a4967033d94fe8a05026b92d4
Murodjon Tokhirov <murodtokh1997@gmail.com>
2023-12-16 17:15:55 +0500

    Closure function

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
+def counter(start):
+    def increment(step=1):
+        nonlocal start
+        start = start +step
+        print(start)
+    return increment
 
-text = """Decimal “is based on a floating-point model which was designed with people in mind, and necessarily has a paramount guiding principle – computers must provide an arithmetic that works in the same way as the arithmetic that 
-people learn at school.” – excerpt from the decimal arithmetic specification."""
+my_func = counter(5)
 
-for i in text:
-    print(i)
-    
-for i in text:
-    print(i)
-
-
-
-
-
+my_func()
+my_func()
